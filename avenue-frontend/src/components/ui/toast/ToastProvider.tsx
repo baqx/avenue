2f20c479707b34a94df5533afd7d6916dcb6b9ee
Adegbola AbdulBaqee <baqeecodes@gmail.com>
2026-07-05 20:09:51 +0100

    modified toasts to use top

--- avenue-frontend/src/components/ui/toast/ToastProvider.tsx
+++ avenue-frontend/src/components/ui/toast/ToastProvider.tsx
@@ -40,7 +40,7 @@ export function ToastProvider({ children }: { children: ReactNode }) {
   return (
     <ToastContext.Provider value={{ toast: addToast, success, error, info, warning }}>
       {children}
-      <div className="fixed bottom-4 right-4 z-50 flex flex-col gap-2">
+      <div className="fixed top-4 right-4 z-50 flex flex-col gap-2">
         {toasts.map((t) => (
           <Toast key={t.id} id={t.id} title={t.title} message={t.message} type={t.type} onClose={removeToast} />
         ))}
